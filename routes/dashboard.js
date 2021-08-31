784a19febc9337fb00d837681b8aaecf9712dbab
otis wilcox <otiswilcox@gmail.com>
2021-08-31 13:35:16 -0400

    set up dashboard route to backend

---
+++ routes/dashboard.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const authorize = require("../middleware/authorize");
+const pool = require("../db");
+
+router.get("/", authorize, async (req, res) => {
+  try {
+    const user = await pool.query(
+      "SELECT user_name, user_id FROM users WHERE users.user_id = $1",
+      [req.user.id]
+    );
+
+    res.json(user.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server error");
+  }
+});
+
+module.exports = router;
